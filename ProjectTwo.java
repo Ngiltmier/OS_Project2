4ad1b194edf83f398c94bbe728e1b4856896106a
Garrett Brenner <garrebrenna@gmail.com>
2020-03-01 17:42:45 -0500

    fixed some stuff

---
+++ ProjectTwo.java
@@ -0,0 +1,28 @@
+public class ProjectTwo {
+
+    public static void main(String[] args) {
+        int memSize; //total memory size
+        int pageSize; //total page size
+        int numJobs; // total number of jobs allowed
+        int minRuntime; //for random generation of runtime for jobs
+        int maxRuntime;
+        int minMemory; //for random generation of memory for jobs
+        int maxMemory;
+
+        try {
+            memSize = Integer.parseInt(args[0]); //total memory size
+            pageSize = Integer.parseInt(args[1]); //total page size
+            numJobs = Integer.parseInt(args[2]); // total number of jobs allowed
+            minRuntime = Integer.parseInt(args[3]); //for random generation of runtime for jobs
+            maxRuntime = Integer.parseInt(args[4]);
+            minMemory = Integer.parseInt(args[5]); //for random generation of memory for jobs
+            maxMemory = Integer.parseInt(args[6]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("USAGE: Please enter the correct number of arguments");
+            System.out.println("<memSize> <pageSize> <numJobs> <minRunTime> <maxRuntime> <minMemory> <maxMemory>");
+            return;
+        }
+
+        VirtualMemory virtualMemory = new VirtualMemory(memSize, pageSize, numJobs, minRuntime, maxRuntime, minMemory, maxMemory);
+    }
+}
---
+++ VirtualMemory.java
@@ -0,0 +1,27 @@
+// Project 2: Simulating memory
+//Garrett Brenner and Noah Giltmier
+
+
+public class VirtualMemory {
+    int memSize; //total memory size
+    int pageSize; //total page size
+    int numJobs; // total number of jobs allowed
+    int minRuntime; //for random generation of runtime for jobs
+    int maxRuntime;
+    int minMemory; //for random generation of memory for jobs
+    int maxMemory;
+
+    public VirtualMemory(int memory, int page, int jobs, int min_run, int max_run, int min_mem, int max_mem){
+        memSize = memory;
+        pageSize = page;
+        numJobs = jobs;
+        minRuntime = min_run;
+        maxRuntime = max_run;
+        minMemory = min_mem;
+        maxMemory = max_mem;
+
+        if (memSize % pageSize != 0){
+            //throw a custom exception
+        }
+    }
+}
